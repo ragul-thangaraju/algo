a6a8b27b4bdb0bc6fa7805fcbb70f93744bfb0de
Ragul <rthangaraju@concertcare.com>
2022-03-23 21:10:12 +0530

    first commit

---
+++ bubblesort.js
@@ -0,0 +1,47 @@
+const arr1 = [2, 5, 8, 3, 5, 1, 2, 3, 5];
+const arr2 = [2, 6, 5, 8, 3];
+
+const bubbleSort = (arr) => {
+  if (!arr.length) return "invalid input";
+
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let tmp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = tmp;
+      }
+    }
+  }
+
+  return arr;
+};
+
+const bubbleSortBS = (arr) => {
+  if (!arr.length) return "invalid input";
+
+  let checked;
+  do {
+    checked = false;
+
+    console.log("---------------");
+    for (let i = 0; i < arr.length; i++) {
+      console.log(arr[i], arr[i + 1]);
+      if (arr[i] > arr[i + 1]) {
+        let temp = arr[i];
+        arr[i] = arr[i + 1];
+        arr[i + 1] = temp;
+        checked = true;
+      }
+    }
+    console.log("---------------", arr, checked);
+  } while (checked);
+  return arr;
+};
+
+console.time();
+console.log(bubbleSort(arr1));
+console.timeEnd();
+console.time();
+console.log(bubbleSortBS(arr2));
+console.timeEnd();
